f151d88c627a797ef1180a2de4beca0f1812911d
Jordan Greissman <jordan@omni.co>
2025-12-11 12:30:42 -0500

    tweaks

--- config/config.js
+++ config/config.js
@@ -71,7 +71,7 @@ let config = {
 				lon: -77.0369,
 				appendLocationNameToHeader: false,
 				roundTemp: true,
-				maxEntries: 8,
+				maxEntries: 10,
 				fade: false,
 				tableClass: "large",
 				tempColors: { cold: 40, warm: 70 }
